c54bae74d07acd4ad58bcc904fad9f5068e39558
Joe-W <80954812+J-Wll@users.noreply.github.com>
2023-10-21 16:55:52 +0100

    Added a title for each module

--- src/App.js
+++ src/App.js
@@ -7,19 +7,25 @@ function App() {
       <div className="module-container">
         <div className="row"> {/* row could be a component that renders x amount of modules based on props, module container could do the same thing rendering x rows of x modules*/}
           <Module
-            purpose="notes" />
+            purpose="notes" 
+            title="Notes" />
           <Module
-            purpose="kanban" />
+            purpose="kanban"
+            title="Kanban Board" />
           <Module
-            purpose="reflective" />
+            purpose="reflective"
+            title="Reflective Journal" />
         </div>
         <div className="row">
           <Module
-            purpose="wireframe" />
+            purpose="wireframe"
+            title="Wireframes" />
           <Module
-            purpose="gitTrack" />
+            purpose="gitStatus"
+            title="Git Status" />
           <Module
-            purpose="aiChat" />
+            purpose="aiChat"
+            title="Chat With AI" />
         </div>
       </div>
     </div>
--- src/Module.js
+++ src/Module.js
@@ -1,16 +1,18 @@
 import { useState, useEffect } from "react";
 
-export default function Module(purpose="undefined") {
-    console.log(purpose);
+export default function Module(props) {
+    console.log(props);
     let [open, updateOpen] = useState(true);
     function checkStatus() {
         if (open) {
             return (<div className="module">
                 <div className="menu-bar">
+                    {console.log(props.title)}
+                    <p className="module-title">{props.title}</p>
                     <button onClick={closeMod} className="close-module">X</button>
                 </div>
                 <div className="module-body">
-                    {componentPurpose(purpose)}
+                    {componentPurpose(props.purpose)}
                 </div>
             </div>)
         }
@@ -21,8 +23,7 @@ export default function Module(purpose="undefined") {
     }
 
     function componentPurpose(purpose){
-        purpose=purpose.purpose;
-        // probably check this in a dict, one side component names and one components
+        // probably check purpose in a dict, one side component names and one components, each component in its own file
 
         if (purpose=="notes"){
             // return(<input type="text" placeholder="Text here">js</input>)
