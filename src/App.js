d5f074cdb2d1ad110f0a384bf3db429b4c5a5523
Joe-W <80954812+J-Wll@users.noreply.github.com>
2023-10-21 14:40:40 +0100

    Cleaning up unnecessary files
    Removed default create react app stuff

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>modified</p>
-      </header>
+    <div class="container">
+      <div class="module-container">
+        <div class="row">
+          <div class="module">
+            <div class="menu-bar">
+              <button class="close-module">X</button>
+            </div>
+          </div>
+          <div class="module">
+            <div class="menu-bar">
+              <button class="close-module">X</button>
+            </div>
+          </div>
+          <div class="module">
+            <div class="menu-bar">
+              <button class="close-module">X</button>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="module">
+            <div class="menu-bar">
+              <button class="close-module">X</button>
+            </div>
+          </div>
+          <div class="module">
+            <div class="menu-bar">
+              <button class="close-module">X</button>
+            </div>
+          </div>
+          <div class="module">
+            <div class="menu-bar">
+              <button class="close-module">X</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
