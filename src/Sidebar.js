7fe9714a04d49e76d764c55fbdfd364ee4a1c8e0
Joe-W <80954812+J-Wll@users.noreply.github.com>
2023-10-21 17:38:30 +0100

    Changed default module sizes

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,6 +3,8 @@ import "./Sidebar.css"
 
 export default function Sidebar() {
     return (
+        // these buttons could check how many in row, if already 3 create new row, else add to existing row
+
         <div className="Sidebar">
             <button>Close Sidebar</button>
             <button>Add Notes</button>
