df0480b6de30b86a6bfa338ef427f9856832fa94
Joe-W <80954812+J-Wll@users.noreply.github.com>
2023-10-22 10:19:49 +0100

    === instead of ==

--- src/Module.js
+++ src/Module.js
@@ -28,7 +28,7 @@ export default function Module(props) {
     function componentPurpose(purpose){
         // probably check purpose in a dict, one side component names and one components, each component in its own file
 
-        if (purpose=="notes"){
+        if (purpose==="notes"){
             // return(<input type="text" placeholder="Text here">js</input>)
             return (<textarea
             type="text"
