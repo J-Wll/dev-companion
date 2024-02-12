975eeef1c4daa74b91ce511307d81df36852abd5
Joe-W <80954812+J-Wll@users.noreply.github.com>
2024-02-12 16:14:17 +0000

    Todo list scaffolding and basic styling

--- src/TodoContent.jsx
+++ src/TodoContent.jsx
@@ -1,7 +1,32 @@
+import { useState } from 'react'
+import './css/TodoContent.css'
+
 export default function TodoContent() {
+    const [todoList, setTodoList] = useState([]);
     return (
         <section className="todo-content">
-
+            <TodoItem text="Do this" />
+            <TodoItem text="Do this" />
+            <TodoItem text="Do this" />
+            <TodoItem text="Do this" />
+            <TodoItem text="Do this" />
+            <TodoItem text="Do this" />
+            <TodoItem text="Do this" />
+            <TodoItem text="Do this" />
+            <TodoItem text="Do this" />
+            <TodoItem text="Do this" />
+            <TodoItem text="Do this" />
+            <TodoItem text="Do this" />
+            <TodoItem text="Do this" />
+            <TodoItem text="Do this" />
         </section>
+    )
+}
+
+function TodoItem(props) {
+    return (
+        <div className="todo-item">
+            <p>{props.text}</p>
+        </div>
     )
 }
