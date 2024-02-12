79ff4f660d097572e17d375c506be7e4621ef486
Joe-W <80954812+J-Wll@users.noreply.github.com>
2024-02-12 18:55:57 +0000

    Added ability to delete to-dos

--- src/TodoContent.jsx
+++ src/TodoContent.jsx
@@ -19,17 +19,18 @@ export default function TodoContent(props) {
         setTodoList((prev) => [...prev, { key: self.crypto.randomUUID(), data: "" }]);
     }
 
-    function updateTodoText(newVal, key) {
-        console.log(newVal);
-        console.log(key);
-        setTodoList((prev) => prev.map((todo) => todo.key === key ? { ...todo, data: newVal } : todo));
-        console.log(todoList);
+    function updateTodoText(newVal, iKey) {
+        setTodoList((prev) => prev.map((todo) => todo.key === iKey ? { ...todo, data: newVal } : todo));
+    }
+
+    function deleteTodo(iKey) {
+        setTodoList((prev) => prev.filter((todo) => todo.key !== iKey));
     }
 
     function getTodos() {
         // Prevents an error from trying to map todolist when it is still undefined
         if (todoList) {
-            return (todoList.map((todo) => <TodoItem key={todo.key} counter={todo.key} text={todo.data} updateTodoText={updateTodoText} />))
+            return (todoList.map((todo) => <TodoItem key={todo.key} counter={todo.key} text={todo.data} updateTodoText={updateTodoText} deleteTodo={deleteTodo} />))
         }
     }
 
@@ -42,10 +43,10 @@ export default function TodoContent(props) {
 }
 
 function TodoItem(props) {
-
     return (
         <div className="todo-item">
             <input type="text" placeholder="What would you like to do?" value={props.text} onChange={(e) => props.updateTodoText(e.target.value, props.counter)} />
+            <button className="delete-todo" onClick={() => props.deleteTodo(props.counter)}>X</button>
         </div>
     )
 }
