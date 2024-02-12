d074c96304524c21fb88e9aa0efd915798384d8a
Joe-W <80954812+J-Wll@users.noreply.github.com>
2024-02-12 18:25:42 +0000

    Added input to Todo module

--- src/TodoContent.jsx
+++ src/TodoContent.jsx
@@ -3,30 +3,35 @@ import './css/TodoContent.css'
 
 export default function TodoContent() {
     const [todoList, setTodoList] = useState([]);
+
+    function addTodo() {
+        setTodoList((prev) => [...prev, { key: self.crypto.randomUUID(), data: "-" }]);
+    }
+
+    function updateTodoText(newVal, key) {
+        console.log(newVal);
+        console.log(key);
+        setTodoList((prev) => prev.map((todo) => todo.key === key ? { ...todo, data: newVal } : todo));
+        console.log(todoList);
+    }
+
+    function getTodos() {
+        return (todoList.map((todo) => <TodoItem key={todo.key} counter={todo.key} text={todo.data} updateTodoText={updateTodoText} />))
+    }
+
     return (
         <section className="todo-content">
-            <TodoItem text="Do this" />
-            <TodoItem text="Do this" />
-            <TodoItem text="Do this" />
-            <TodoItem text="Do this" />
-            <TodoItem text="Do this" />
-            <TodoItem text="Do this" />
-            <TodoItem text="Do this" />
-            <TodoItem text="Do this" />
-            <TodoItem text="Do this" />
-            <TodoItem text="Do this" />
-            <TodoItem text="Do this" />
-            <TodoItem text="Do this" />
-            <TodoItem text="Do this" />
-            <TodoItem text="Do this" />
+            <button onClick={addTodo}>Add To-do Item</button>
+            {getTodos()}
         </section>
     )
 }
 
 function TodoItem(props) {
+
     return (
         <div className="todo-item">
-            <p>{props.text}</p>
+            <input type='text' value={props.text} onChange={(e) => props.updateTodoText(e.target.value, props.counter)} />
         </div>
     )
 }
