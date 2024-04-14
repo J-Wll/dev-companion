ec0d5f6fe20c828502240408cd708bd1d9e6b7a5
Joe-W <80954812+J-Wll@users.noreply.github.com>
2024-04-14 20:50:57 +0100

    more kanban implementation

--- src/module-contents/KanbanContent.jsx
+++ src/module-contents/KanbanContent.jsx
@@ -1,19 +1,41 @@
+import { useState, useEffect } from "react";
 import Draggable from 'react-draggable';
 
 import "../css/css-module-content/kanbanContent.css"
 
-export default function KanbanContent() {
+function KanbanColumn(props) {
+    return (
+        <div className='kanban-column'>
+            <div className="kanban-header">
+                {props.name}
+                <div>
+                    <button>+</button>
+                    <button>edit</button>
+                    <button>x</button>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default function KanbanContent(props) {
+    const [localData, setLocalData] = useState(props.dataFromGlobal);
+
+    useEffect(() => {
+        if (localData) {
+            props.setData(props.counter, localData)
+        }
+    }, [localData])
+
     return (
         <>
-            <div className='kanban-column'>
+            <KanbanColumn name="do" />
+            <KanbanColumn name="doing" />
+            <KanbanColumn name="done" />
 
-            </div>
-            <div className='kanban-column'>
+            <button>+</button>
 
-            </div>
-            <div className='kanban-column'>
-
-            </div>
             <Draggable bounds={"parent"} axis={"both"} grid={[200, 1]}>
                 <span className='kanban-item'>
                     <button>-</button>
