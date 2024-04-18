3e243a1d691ab2e5fe0e3006d0c0f9f178b75e31
Joe-W <80954812+J-Wll@users.noreply.github.com>
2024-04-18 15:45:40 +0100

    Delete capabilities to resources and styling

--- src/Module.jsx
+++ src/Module.jsx
@@ -63,6 +63,7 @@ export default function Module(props) {
         case "Resources":
             component = <ResourcesContent {...componentProps} />;
             title = "Resources";
+            defaultSize = ["450px", "280px"];
             break;
         default:
             component = <></>;
--- src/module-contents/ResourcesContent.jsx
+++ src/module-contents/ResourcesContent.jsx
@@ -3,22 +3,17 @@ import { useState, useEffect, useRef } from "react";
 import "../css/css-module-content/ResourcesContent.css"
 import defaultResources from "../assets/Default Resources.json";
 
-function Resources({ selected, data }) {
-    console.log(data.data[selected]);
+function Resources(props) {
+    console.log(props.data.data[props.selected]);
 
-
-    // window.electron.openLink("https://stackoverflow.com/questions/73464422/electron-app-how-do-i-open-link-in-browser");
     try {
-
-        const returnVal = data.data[selected].map((item, index) =>
-            <li key={index}><a href="#" onClick={() => window.electron.openLink(item.link)}>{item.name}</a> - {item.description}</li>
+        const returnVal = props.data.data[props.selected].map((item, index) =>
+            <li key={index}><a href="#" onClick={() => window.electron.openLink(item.link)}>{item.name}</a> - {item.description} <button onClick={() => { props.deleteResource(item.name) }} className="delete-resource">X</button></li>
         )
         return (
-            // <></>
             returnVal
         )
     }
-
     catch (error) {
         console.log(error);
     }
@@ -28,6 +23,17 @@ export default function ResourcesContent(props) {
     const [localData, setLocalData] = useState(props.dataFromGlobal);
     const [options, setOptions] = useState([])
     const [refresh, triggerRefresh] = useState(false);
+
+    function deleteResource(dlName) {
+        const newld = localData;
+        const filteredContent = newld.data[localData.selected]
+            .filter((e) => {
+                return dlName != e.name;
+            })
+        newld.data[localData.selected] = filteredContent;
+        setLocalData(newld);
+        triggerRefresh(!refresh);
+    }
 
     useEffect(() => {
         if (localData) {
@@ -60,11 +66,11 @@ export default function ResourcesContent(props) {
         return (
             <>
                 <div>
-                    <label htmlFor="resource-category-input">Select workspace:</label>
+                    <label htmlFor="resource-category-input">Select resources: </label>
                     <select value={localData.selected} onChange={(e) => { setLocalData({ ...localData, selected: e.target.value }) }} name="resource-category-input" id="resource-category-input" >
                         {options}
                     </select>
-                    <Resources selected={localData.selected} data={localData} />
+                    <Resources selected={localData.selected} data={localData} deleteResource={deleteResource} />
                 </div>
             </>
         )
