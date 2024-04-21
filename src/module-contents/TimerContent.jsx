4f9ac0d5cd4c9f596a5e89f3f89dfd7d2af4f4a6
Joe-W <80954812+J-Wll@users.noreply.github.com>
2024-04-21 15:42:43 +0100

    Update TimerContent.jsx

--- src/module-contents/TimerContent.jsx
+++ src/module-contents/TimerContent.jsx
@@ -17,7 +17,6 @@ export default function TimerContent(props) {
             setLocalData(() => ({ timerActive: true, time: 0, interval: 10, notifies: false, repeats: false, pomodoro: { on: false, workMode: true, workInterval: 45, restInterval: 15 } }));
         }
         if (typeof localData === "object" && "label" in localData) {
-            console.log(localData.label, labelRef.current);
             labelRef.current.value = localData.label;
         }
     }, [])
